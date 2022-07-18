dedf9587124fc14707d41d5a30a5ec09736558ae
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-07-18 20:28:59 +0300

    Added Meals components

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,14 @@
+import classes from "./AvailableMeals.module.css";
+import DUMMY_MEALS from "./DummyMealsData";
+
+const AvailableMeals = () => {
+    return (
+        <section className={classes.meals}>
+            <ul>
+                {DUMMY_MEALS.map(meal => <li>{meal.name}</li>)}
+            </ul>
+        </section>
+    );
+};
+
+export default AvailableMeals;
