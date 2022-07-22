b2f955a136d823b9ec74521e83fd99b279befe7e
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-07-22 19:51:29 +0300

    Using context in CartButton component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+
 import classes from "./HeaderCartButton.module.css";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount
+    }, 0)
+
     return (
         <button className={classes.button} onClick={props.onClickCart}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your cart</span>
-            <span className={classes.badge}>5</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
