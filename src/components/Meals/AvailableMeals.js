dbed59ad3632aa8c11904bc5411f42e0a49cb145
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-07-23 14:56:49 +0300

    Add order amount limit

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,18 +4,20 @@ import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 import DUMMY_MEALS from "./DummyMealsData";
 
-
 const AvailableMeals = () => {
     return (
         <section className={classes.meals}>
             <Card>
                 <ul>
-                    {DUMMY_MEALS.map(meal => 
-                    <MealItem key={meal.id} 
-                    name={meal.name} 
-                    description={meal.description}
-                    price={meal.price}
-                    />)}
+                    {DUMMY_MEALS.map((meal) => (
+                        <MealItem
+                            key={meal.id}
+                            id={meal.id}
+                            name={meal.name}
+                            description={meal.description}
+                            price={meal.price}
+                        />
+                    ))}
                 </ul>
             </Card>
         </section>
